6b7bb386939bfcb911cd1771046e64309967c77c
amstevenson <addstevenson@hotmail.com>
2023-03-20 13:56:38 +0000

    cleanup

--- dev_env/setup_files/service.py
+++ dev_env/setup_files/service.py
@@ -2,25 +2,29 @@ from dev_env.setup_files.repo_setup import create_repo_if_required, copy_script_
 from dev_env.setup_files.logging.logger import logger
 from os import walk
 
-def setup_service(service, dir :str, scripts_required: list, env_var_substitutions: dict, key_vault: str, service_type: str, all_docker_per_service: bool):
+def setup_service(service, dir :str, all_docker_per_service: bool):
     logger.info('Installing %s' % (service['name']))
-    orchestrate_service_setup(service, dir, scripts_required, env_var_substitutions, key_vault, service_type, all_docker_per_service)
+    orchestrate_service_setup(service, dir, all_docker_per_service)
     logger.info('Finished installing %s' % (service['name']))
 
-def orchestrate_service_setup(service, dir :str, scripts_required: list, env_var_substitutions: dict, key_vault: str, service_type: str, all_docker_per_service: bool):
+def orchestrate_service_setup(service, dir :str, all_docker_per_service: bool):
     service_name = service['name']
+    service_type = service['type']
+    serivce_env_var_subs = service['envVarSubstitutions'] if 'envVarSubstitutions' in service else {}
+    service_key_vault = service['keyVault'] if 'keyVault' in service else ''
+    scripts_required = service['scriptsRequired'] if 'scriptsRequired' in service else []
     file_path_of_service = '%s/dev_env/apps/%s' % (dir, service_name)
 
     create_repo_if_required(service_name, file_path_of_service, service['gitUrl'])
     copy_script_files(dir, service_name, scripts_required) \
         if (len(scripts_required) > 0) \
         else logger.info('No scripts found for %s' % (service_name))
-    copy_environment_vars(dir, env_var_substitutions, service_name) \
-        if (len(env_var_substitutions) > 0) \
+    copy_environment_vars(dir, serivce_env_var_subs, service_name) \
+        if (len(serivce_env_var_subs) > 0) \
         else logger.info('No env vars found for %s' % (service_name))
     run_script_files('%s/bin' % (file_path_of_service), 
         next(walk('%s/bin' % (file_path_of_service)), (None, None, []))[2], 
         service_name, 
-        key_vault, 
+        service_key_vault, 
         service_type, 
         all_docker_per_service)
--- start.py
+++ start.py
@@ -5,29 +5,44 @@ from dev_env.setup_files.service import setup_service
 from dev_env.setup_files.utils.utils import query_yes_no
 from dev_env.setup_files.utils.prompts import run_db_only
 
-def setup_services(all_docker_per_service: bool):
+def setup_all_services(all_docker_per_service: bool):
     with open('services.json') as file:
         for service in json.load(file)['services']: 
             setup_service(service, 
-                os.path.dirname(os.path.realpath(__file__)), 
-                service['scriptsRequired'] if 'scriptsRequired' in service else [], 
-                service['envVarSubstitutions'] if 'envVarSubstitutions' in service else {},
-                service['keyVault'] if 'keyVault' in service else {}, 
-                service['type'] if 'type' in service else '', 
+                os.path.dirname(os.path.realpath(__file__)),
                 all_docker_per_service)
+
+def setup_one_service(all_docker_per_service: bool, service_name: str):
+    print(all_docker_per_service)
+
+    with open('services.json') as file:
+        service_json = [a for a in json.load(file)['services'] if a['name']==service_name]
+
+
+
+        print(service_json)
 
 def determine_action_based_on_command(command: str):
     # Run all services: no args, default
     if len(command) == 0 and query_yes_no('Do you want to clone and install all services?'):
-        setup_services('y') if run_db_only() else setup_services('n')
+        setup_all_services('y') if run_db_only() else setup_all_services('n')
+        quit()
 
     # Run all services: specific args: [start.py start service all]
     elif len(command) == 3 and 'start' in command and 'service' in command and 'all' in command:
-        setup_services('y') if run_db_only() else setup_services('n')
+        setup_all_services('y') if run_db_only() else setup_all_services('n')
+        quit()
+
+    # Run specific service: specific args: [start.py start service <specific service>]
+    elif len(command) == 3 and 'start' in command and 'service':
+        setup_one_service('y', command[2]) if run_db_only() else setup_one_service('n', command[2])
+        quit()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run arguments for dev-env')
-    parser.add_argument('command', nargs='*', default=[], help='%s | %s' % ( \
+    parser.add_argument('command', nargs='*', default=[], help='%s | %s | %s' % ( \
         '1) start service all (start all services)', 
-        '2) <no args> (default, prompt to start all services)'))
+        '2) start service <service> (start specific service - i.e bulk-scan-orchestrator)',
+        '3) <no args> (default, prompt to start all services)'))
+    parser.add_argument('-services', '-start', help='old foo help')
     determine_action_based_on_command(vars(parser.parse_args())['command'])
