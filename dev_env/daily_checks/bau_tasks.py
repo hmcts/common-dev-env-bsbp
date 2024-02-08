eec438b7b7ab1468f8113a49156c515d7826f5da
amstevenson <addstevenson@hotmail.com>
2024-02-08 13:04:43 +0000

    adding section to delete unabortable or retryable envelope

--- dev_env/daily_checks/bau_tasks.py
+++ dev_env/daily_checks/bau_tasks.py
@@ -3,20 +3,21 @@ import time
 from datetime import datetime, timedelta, timezone
 from dev_env.setup_files.logging.logger import logger
 
+
 def run_bsp_bau_tasks(env: str):
-    """ 
+    """
     Usage: python3 start.py run dailychecks <env>
 
     1) Check health for services
 
-    2) Check stale envelopes in blob router - this should always be empty. In the case it isn't, 
+    2) Check stale envelopes in blob router - this should always be empty. In the case it isn't,
     add an action to the list
 
-    3) Stale envelopes in processor - should be empty, if not then do a PUT request to make 
+    3) Stale envelopes in processor - should be empty, if not then do a PUT request to make
     the envelope reprocess itself as an exception record so that a caseworker can investigate further
 
     4) Stale letters in bulk print - depending on the state, call abort or complete
-    
+
     """
     # Prompt the user for the Authorization token and set headers
     headers = {"Authorization": input("Enter your Authorization token (Bearer token): ")}
@@ -25,8 +26,43 @@ def run_bsp_bau_tasks(env: str):
     check_stale_blobs(actions, env)
     handle_stale_letters(headers, actions, env)
     reprocess_stale_envelopes(headers, actions, env)
+    handle_unprocessable_stale_envelopes(headers, actions, env)
     if len(actions) > 0:
         logger.info(f"Actions to look into are: {actions}" if len(actions) > 0 else "No actions; all looks good!")
+
+
+def handle_unprocessable_stale_envelopes(headers: dict, actions: list, env:str):
+    # Define the base URL of the GET endpoint to retrieve initial data
+    initial_data_url = f"http://bulk-scan-processor-{env}.service.core-compute-{env}.internal/envelopes/stale-incomplete-envelopes"
+
+    # Define the base URL of the DELETE endpoint
+    base_url = f"http://bulk-scan-processor-{env}.service.core-compute-{env}.internal/envelopes/stale/"
+
+    # Make a GET request to fetch the initial data
+    response = fetch_data_with_retries(initial_data_url)
+
+    # Extract envelope data
+    envelope_data = response["data"]
+
+    for entry in envelope_data:
+        delete_url_full = base_url + entry['envelope_id']
+
+        logger.info('Attempting to remove: letter ' + entry['envelope_id'])
+
+        try:
+            response = requests.delete(delete_url_full, json={}, headers=headers)
+            if response.status_code == 200:
+                # Process the response
+                logger.info(f'Envelope has been removed: {entry["envelope_id"]}')
+            else:
+                if response.status_code == 500:
+                    logger.info(f'Envelope has NOT been removed: {entry["envelope_id"]}')
+                    actions.append(f'entry["envelope_id"] failed to be removed as a stale-letter, payment DCN found.')
+                else:
+                    response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print(e)
+
 
 def check_stale_blobs(actions: list, env: str):
     stale_blob_url = f"http://reform-scan-blob-router-{env}.service.core-compute-{env}.internal/stale-blobs?stale_time=20"
@@ -34,9 +70,10 @@ def check_stale_blobs(actions: list, env: str):
     if envelopes["count"] > 0:
         actions.append(f"Investigate stale blobs: {stale_blob_url}")
 
+
 def check_services_health(actions: list, env: str):
-    urls_to_check = [f"http://bulk-scan-processor-{env}.service.core-compute-{env}.internal/health", 
-                     f"http://bulk-scan-orchestrator-{env}.service.core-compute-{env}.internal/health", 
+    urls_to_check = [f"http://bulk-scan-processor-{env}.service.core-compute-{env}.internal/health",
+                     f"http://bulk-scan-orchestrator-{env}.service.core-compute-{env}.internal/health",
                      f"http://bulk-scan-payment-processor-{env}.service.core-compute-{env}.internal/health"]
 
     for service_url in urls_to_check:
@@ -44,10 +81,11 @@ def check_services_health(actions: list, env: str):
         if not health_response == "UP":
             actions.append(f"Check service (showing as {health_response}): {service_url}")
 
+
 def handle_stale_letters(headers: dict, actions: list, env:str):
     # Fetch the JSON data from the URL
     url = f"http://rpe-send-letter-service-{env}.service.core-compute-{env}.internal/stale-letters"
-    
+
     # Fetching the data for bulk print sometimes gives a 502 for some reason, so retry a few times
     response = fetch_data_with_retries(url)
     data = response['stale_letters']
@@ -80,6 +118,7 @@ def handle_stale_letters(headers: dict, actions: list, env:str):
             actions.append(f"Stale letter: {letter_id}, {created_at_str}")
             print(f"Failed to mark letter with ID {letter_id}. Status code: {response.status_code}")
 
+
 def reprocess_stale_envelopes(headers: dict, actions: list, env: str):
     # Define the base URL of the GET endpoint to retrieve initial data
     initial_data_url = f"http://bulk-scan-processor-{env}.service.core-compute-{env}.internal/envelopes/stale-incomplete-envelopes"
@@ -99,7 +138,7 @@ def reprocess_stale_envelopes(headers: dict, actions: list, env: str):
         envelope_id = entry["envelope_id"]
         container = entry["container"]
         file_name = entry["file_name"]
-        
+
         # Construct the URL for the specific envelope ID
         url = base_url + envelope_id
         new_url = f"http://bulk-scan-processor-{env}.service.core-compute-{env}.internal/envelopes/{container}/{file_name}"
@@ -107,7 +146,7 @@ def reprocess_stale_envelopes(headers: dict, actions: list, env: str):
 
         url = base_url + envelope_id + "/abort" if notification_sent_status else \
             base_url + "reprocess/" + envelope_id
-        
+
         logger.info("Attempting to " + "abort..." if notification_sent_status else "reprocess...")
 
         # Make the PUT request with the Authorization header
@@ -116,7 +155,7 @@ def reprocess_stale_envelopes(headers: dict, actions: list, env: str):
         # Check if the PUT request was successful (HTTP status code 200) and confirm CCD_ID populated if reprocess called
         if put_response.status_code == 200 and not notification_sent_status:
             logger.info(f"PUT request successful for envelope ID: {envelope_id}")
-            
+
             # Initialize a timer
             start_time = time.time()
             timeout = 20  # 20 seconds timeout
