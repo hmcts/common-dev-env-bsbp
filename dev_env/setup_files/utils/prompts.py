8fa6974d5525a60256243b96e94dfa0f4ee91b9d
amstevenson <addstevenson@hotmail.com>
2024-07-11 16:51:16 +0100

    changing message

--- dev_env/setup_files/utils/prompts.py
+++ dev_env/setup_files/utils/prompts.py
@@ -1,5 +1,8 @@
 from dev_env.setup_files.utils.utils import query_yes_no
 
+
 def run_db_only():
-    return query_yes_no('%s %s' % ('For each service, do you want to only spin up the database?',
-            'y = db only, n = all docker services listed in docker compose yml'))
+    return query_yes_no('%s %s %s %s' % ('For each service, will you be running them in an IDE? ',
+                                         'Do you want to only spin up the database?',
+                                         'y = db only (intended for IDE running),',
+                                         'n = all docker services listed in docker compose yml'))
